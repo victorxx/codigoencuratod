637e45c6d179f076ce969f75cf2706cb87cadd7c
victorxx <victorsaib@gmail.com>
2025-07-22 19:33:28 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,31 +1,32 @@
 import time
-import random
 from playwright.sync_api import sync_playwright
 
 urls = [
-    "https://example.com",
-    "https://4br.me/FmnTOjH",
-    "https://google.com"
+    "https://4br.me/r3F19LoX",
+
 ]
 
-def run_playwright_on_urls(url_list):
+def run():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        for url in url_list:
+        index = 0
+        while True:
+            url = urls[index % len(urls)]  # pega um link por vez, em ordem circular
             print(f"Abrindo {url}")
+            
+            browser = p.chromium.launch(headless=False)
+            page = browser.new_page()
             page.goto(url, timeout=60000)
             page.wait_for_load_state('networkidle', timeout=60000)
-            wait_time = random.randint(120, 300)  # espera entre 2 e 5 minutos
-            print(f"Esperando {wait_time} segundos na página para parecer natural...")
-            time.sleep(wait_time)
-        print("Aguardando 15 segundos antes de fechar o navegador devagar...")
-        time.sleep(15)
-        browser.close()
-        print("Navegador fechado.")
+            
+            print("Esperando 3 minutos na página para parecer natural...")
+            time.sleep(3 * 60)  # 3 minutos
+            
+            browser.close()
+            print("Fechado navegador.")
+
+            index += 1
+            print("Esperando 24 horas para próxima visita...")
+            time.sleep(24 * 60 * 60)  # espera 24 horas
 
 if __name__ == "__main__":
-    while True:
-        run_playwright_on_urls(urls)
-        print("Esperando 24 horas para próxima rodada...")
-        time.sleep(24 * 60 * 60)
+    run()
